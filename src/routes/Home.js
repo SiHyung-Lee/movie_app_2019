9c22dd589757cebef13c64f7bcf5a09abe3aa142
isihyeong <buzzcatfish@gmail.com>
2020-03-11 00:35:04 +0900

    set router

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from '../components/Movie';
-import './Home.css';
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
 
 class Home extends React.Component {
     state = {
@@ -14,7 +14,7 @@ class Home extends React.Component {
             data: {
                 data: { movies }
             }
-        } = await axios.get('https://yts.mx/api/v2/list_movies.json');
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json");
         this.setState({
             isLoading: false,
             movies
@@ -27,6 +27,7 @@ class Home extends React.Component {
 
     render() {
         const { isLoading, movies } = this.state;
+
         return (
             <section className="container">
                 {isLoading ? (
